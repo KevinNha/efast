1faf09b520121d5b9ce8b900861f19524c7d0c5a
Hyunjin Nha <hyunjinnha@gmail.com>
2021-01-09 16:03:45 -0800

    Added Drawer

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import { StyleSheet, Text, View } from 'react-native';
+
+import ChannelScreen from './src/components/ChannelScreen';
+import ChannelListDrawer from './src/components/ChannelListDrawer';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Drawer.Navigator
+            drawerContent={ChannelListDrawer}
+            drawerStyle={styles.drawerNavigator}>
+            <Drawer.Screen name="ChannelScreen" component={ChannelScreen} />
+          </Drawer.Navigator>
+        </View>
+      </NavigationContainer>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -14,8 +31,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  drawerNavigator: {
+    backgroundColor: 'black',
+    width: 350,
+    color: 'white',
+  },
+  channelScreenSaveAreaView: {
+    backgroundColor: 'white',
+  },
+  channelScreenContainer: {flexDirection: 'column', height: '98%'}
 });
---
+++ src/components/ChannelScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+function ChannelScreen({navigation, route}) {
+  return (
+    <SafeAreaView>
+      <Text>Channel Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default ChannelScreen
