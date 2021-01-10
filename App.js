10649c19019241d6e07e6bdbd74a760bec9f8aef
JK Kim <jk.jonggyuk@gmail.com>
2021-01-10 12:42:06 -0800

    merge conflict resolve

--- App.js
+++ App.js
@@ -77,31 +77,9 @@ export default function App() {
             <Stack.Screen name="Dashboard" component={ChannelNav} />
             <Stack.Screen name="Chat" component={Chat} />
           </Stack.Navigator>
-<<<<<<< HEAD
         {/* ) : (
 
         )} */}
-=======
-        ) : (
-          // <Stack.Navigator
-          //   initialRouteName="Splash">
-          //   <Stack.Screen name="Splash" component={Splash} />
-          //   <Stack.Screen name="Login" component={Login} />
-          //   <Stack.Screen name="SignUp" component={SignUp} />
-          //   <Stack.Screen name="Dashboard" component={Dashboard} />
-          //   <Stack.Screen name="Chat" component={Chat} />
-          // </Stack.Navigator>
-          <View style={styles.container}>
-          <Drawer.Navigator
-            drawerContent={ChannelListDrawer}
-            drawerStyle={styles.drawerNavigator}>
-            <Drawer.Screen name="ChannelScreen" component={ChannelScreen} />
-            <Drawer.Screen name="Dashboard" component={Dashboard} />
-            <Drawer.Screen name="Chat" component={Chat} />
-          </Drawer.Navigator>
-        </View>
-        )}
->>>>>>> f6a3d3f457a1478b0f9c90c062c7d35b7877f239
       </NavigationContainer>
       {/* <Text>Open up App.js to start working on your app!</Text> */}
       {/* <StatusBar style="auto" /> */}
